8b8f46461490797aeae13a7f07c1484b58ff83c5
Josh Hays <josh.hays@microsoft.com>
2021-08-17 15:34:50 -0400

    Stubbed out the GetRating and GetRatings functions

---
+++ src/IceCreamRatingsService/GetRating.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using IceCreamRatingsService.Models;
+
+namespace IceCreamRatingsService
+{
+    public static class GetRating
+    {
+        [FunctionName("GetRating")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string ratingId = req.Query["ratingId"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            ratingId = ratingId ?? data?.ratingId;
+
+            Rating rating = new Rating();
+
+            return new OkObjectResult(rating);
+        }
+    }
+}
---
+++ src/IceCreamRatingsService/GetRatings.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+using IceCreamRatingsService.Models;
+
+namespace IceCreamRatingsService
+{
+    public static class GetRatings
+    {
+        [FunctionName("GetRatings")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string userId = req.Query["userId"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            userId = userId ?? data?.userId;
+
+            List<Rating> ratings = new List<Rating>();
+
+
+            return new OkObjectResult(ratings);
+        }
+    }
+}
