42dd6d9140d0662032b2a4eb2bfbeee9f4e4d0aa
JGutherie <jgutherie@microsoft.com>
2021-08-17 15:27:33 -0400

    Added new models

---
+++ src/IceCreamRatingsService/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace IceCreamRatingsService.Models
+{
+	public class Product
+	{
+		[JsonProperty("productId")]
+		public Guid ProductId { get; set; }
+		[JsonProperty("productName")]
+		public String ProductName { get; set; }
+		[JsonProperty("productDescription")]
+		public String ProductDescription { get; set; }
+	}
+}
---
+++ src/IceCreamRatingsService/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace IceCreamRatingsService.Models
+{
+	public class User
+	{
+		[JsonProperty("userId")]
+		public Guid UserId { get; set; }
+		[JsonProperty("userName")]
+		public String UserName { get; set; }
+		[JsonProperty("fullName")]
+		public String FullName { get; set; }
+	}
+}
