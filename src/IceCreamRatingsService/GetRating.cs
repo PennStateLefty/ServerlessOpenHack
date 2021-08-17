10a50efe9c0e5e5a1666b43b0a9a31a00e737568
Josh Hays <josh.hays@microsoft.com>
2021-08-17 17:04:46 -0400

    fixed complier warning on GetRating function.  Removed async since the input binding did not support that for this case.

--- src/IceCreamRatingsService/GetRating.cs
+++ src/IceCreamRatingsService/GetRating.cs
@@ -3,21 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 using IceCreamRatingsService.Models;
-using Microsoft.Azure.Documents.Client;
-using System;
-using Microsoft.Azure.Documents.Linq;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace IceCreamRatingsService
 {
     public static class GetRating
     {
         [FunctionName("GetRating")]
-        public static async Task<IActionResult> Run(
+        public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "rating/{id}")] HttpRequest req, string id,
             [CosmosDB(
                 databaseName: "IceCreamRatings",
