9eaf736af62309a0c1c8808b473077ea7c44a847
Josh Hays <josh.hays@microsoft.com>
2021-08-19 00:26:52 -0400

    added http test for durable function to validate locally.

--- src/OrderBatchService/MonitorOrderBatch.cs
+++ src/OrderBatchService/MonitorOrderBatch.cs
@@ -2,7 +2,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -10,50 +9,63 @@ namespace OrderBatchService
 {
     public static class MonitorOrderBatch
     {
-        [FunctionName("MonitorOrderBatch")]
-        public static async Task<List<string>> RunOrchestrator(
-            [OrchestrationTrigger] IDurableOrchestrationContext context)
+        [FunctionName("HttpMonitorOrderBatch")]
+        public static async Task RunMonitorBatch(
+            [OrchestrationTrigger] IDurableOrchestrationContext context,
+            ILogger log)
         {
-            var outputs = new List<string>();
+            string batchId = context.GetInput<string>();
+            log.LogInformation("Received event for batch: " + batchId);
 
-            outputs.Add(await context.CallActivityAsync<string>("MonitorOrderBatch_Hello", "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>("MonitorOrderBatch_Hello", "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>("MonitorOrderBatch_Hello", "London"));
+            var gate1 = context.WaitForExternalEvent("OrderHeaderDetails.csv");
+            var gate2 = context.WaitForExternalEvent("OrderLineItems.csv");
+            var gate3 = context.WaitForExternalEvent("ProductInformation.csv");
 
-            string applicationId = context.GetInput<string>();
+            // All three files must be created before the batch can be processed
+            await Task.WhenAll(gate1, gate2, gate3);
 
-            //var gate1 = context.WaitForExternalEvent("CityPlanningApproval");
-            //var gate2 = context.WaitForExternalEvent("FireDeptApproval");
-            //var gate3 = context.WaitForExternalEvent("BuildingDeptApproval");
-
-            //// all three departments must grant approval before a permit can be issued
-            //await Task.WhenAll(gate1, gate2, gate3);
-
-            //await context.CallActivityAsync("IssueBuildingPermit", applicationId);
-
-            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
-            return outputs;
+            log.LogInformation("Recieved all files for batch: " + batchId);
+            await context.CallActivityAsync("ProcessBatch", batchId);
         }
 
-        [FunctionName("MonitorOrderBatch_Hello")]
-        public static string SayHello([ActivityTrigger] string name, ILogger log)
+        [FunctionName("HttpProcessBatch")]
+        public static void RunProcessBatch(
+            [ActivityTrigger] string batchId,
+            ILogger log)
         {
-            log.LogInformation($"Saying hello to {name}.");
-            return $"Hello {name}!";
+            log.LogInformation($"Processing batchId: {batchId}.");
         }
 
-        [FunctionName("MonitorOrderBatch_HttpStart")]
-        public static async Task<HttpResponseMessage> HttpStart(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
+        [FunctionName("HttpMonitorOrderBatchStart")]
+        public static async Task<HttpResponseMessage> RunHttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "order/{filename}")] HttpRequestMessage req, string filename,
             [DurableClient] IDurableOrchestrationClient starter,
             ILogger log)
         {
-            // Function input comes from the request content.
-            string instanceId = await starter.StartNewAsync("MonitorOrderBatch", null);
+            // Use the file name as the batchId and gate for processing
+            // The prefix is the batchId
+            // The files are the gates
+            string[] fileNameTokens = filename.Split("-");
+            string batchId = fileNameTokens[0];
+            string file = fileNameTokens[1];
 
-            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+            // Check if an instance with the specified ID already exists or an existing one stopped running(completed/failed/terminated).
+            var existingInstance = await starter.GetStatusAsync(batchId);
+            if (existingInstance == null
+            || existingInstance.RuntimeStatus == OrchestrationRuntimeStatus.Completed
+            || existingInstance.RuntimeStatus == OrchestrationRuntimeStatus.Failed
+            || existingInstance.RuntimeStatus == OrchestrationRuntimeStatus.Terminated)
+            {
+                // An instance with the specified ID doesn't exist or an existing one stopped running, create one.
+                await starter.StartNewAsync("HttpMonitorOrderBatch", batchId, batchId);
+                log.LogInformation($"Started orchestration with ID = '{batchId}'.");
+            }
 
-            return starter.CreateCheckStatusResponse(req, instanceId);
+            log.LogInformation("Raising Orchestration Event with batchId=" + batchId + ", fileName=" + filename + ", file=" + file);
+            await starter.RaiseEventAsync(batchId, file, batchId);
+
+            // Return status response
+            return starter.CreateCheckStatusResponse(req, batchId);
         }
     }
 }
